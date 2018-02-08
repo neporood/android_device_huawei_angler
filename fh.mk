902cde7af46f379d7f3f7a4a4e37793db10c15ac
neporood <neporood@live.com>
2018-02-08 06:21:59 -0600

    Dogs Will Hunt!

--- fh.mk
+++ fh.mk
@@ -3,14 +3,15 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
+$(call inherit-product, vendor/nepo/my-apps.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_angler
-PRODUCT_BRAND := google
+PRODUCT_NAME := fh_angler
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6P
 
 TARGET_VENDOR := huawei
@@ -19,3 +20,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
+
